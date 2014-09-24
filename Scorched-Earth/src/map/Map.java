ef5780a4502fbc4485cb33ec5159ace51c33969d
WickedKing1392 <cody.shafer@gmail.com>
2014-09-23 20:17:19 -0700

    Added jar files

--- Scorched-Earth/src/map/Map.java
+++ Scorched-Earth/src/map/Map.java
@@ -1,5 +1,21 @@
 package map;
 
+import java.util.HashMap;
+
 public class Map {
+	
+	HashMap<Integer, Integer> my_map;
+	
+	public Map(){
+		my_map = new HashMap<Integer, Integer>();
+		createMap();
+	}
+	
+	private void createMap(){
+		for(int i = 0; i < 800; i ++){
+			my_map.put(i, 0);
+		}
+		
+	}
 
 }
